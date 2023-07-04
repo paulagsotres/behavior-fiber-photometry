dcc4e3943d7c85f84f9e99d1a0c0930860256fb0
Paula <120389016+paulagsotres@users.noreply.github.com>
2023-07-04 19:08:40 +0200

    Add files via upload

--- configuration.py
+++ configuration.py
@@ -9,26 +9,29 @@ from pathlib import Path
 
 
 # parameters to change
-dir_path = Path(r"C:\Users\pgomez\Desktop\dlx cre") #change to point to folder
-path_summary = r"C:\Users\pgomez\Desktop\dlx cre\Summary.csv" #change to point to summary file for NOR
+dir_path = Path(r"H:\Experiments\2023\20230614- MitoGcaMP6 OB CB1 KO\Odor stress and NOR\TEST PHASE") #change to point to folder
+path_summary = None #change to point to summary file for NOR
 filename_analysis = dir_path.stem + ("_ALL_MICE_analyzed.csv") #if you want to change the output name of the files 
 filename_pdf = dir_path.stem + ("_ALL_MICE_analyzed.pdf") #change to your liking
 filename_behavior = dir_path.stem + ("_ALL_MICE_behavior.pdf")
-name_behav = "*behavior*.csv" #what item do your behavioral files have to identify them
+name_behav = "*BEHAVIOR*.csv" #what item do your behavioral files have to identify them PUT ** AROUND THE WORD
 
 
 #adjust parameters to your experiment (if you dont need them dont change them)
-filter_window = 10 #average moving window to calculate Fmean in frames
+
 framerate = 20 #of data acquired
 photobleach = 1200 #time to remove because of the photobleaching in frames
-baseline = None #simple baseline in frames in case your experiment doesn't have specific behaviors, if specific baseline put None
-type_smoothing = None #or "savitzky" or None if you dont want to smooth
+baseline = 1200 #simple baseline in frames in case your experiment doesn't have specific behaviors, if specific baseline put None
+filter_window = 10 #average moving window to calculate Fmean in frames
+type_smoothing = "moving_average" #or "moving_average or  "savitzky" or None if you dont want to smooth
+window_length = 21 #for savitzky filter
+
 
 #peak detection
 window_size = 2400 #to calculate dynamic MAD instead of in the whole recording (put original number of frames if you want the whole recording)
-threshold = 3 #times MAD
+threshold = 4 #times MAD
 distance = 100 #Required minimal horizontal distance (>= 1) in samples between neighbouring peaks. Smaller peaks are removed first until the condition is fulfilled for all remaining peaks.
-type_detection = "threshold" #vs "threshold"
+type_detection = "prominence" #vs "height"
 num_frames = 25 #to take before and after behavioral event
 
 
@@ -41,15 +44,15 @@ bandpass = {"lowcut": 0.01, "highcut":7, "order": 4} #add None if you dont use
 
 #extract signal for long behaviors
 time_before_onset = 15
-time_exposure = 30
-time_after_onset = 60
+time_exposure = 20
+time_after_onset = 30
 
 #baseline for injections in seconds 
-drug = {"baseline_start": -120, "baseline_end": -60} #baseline is how much time before injection you want to measure before you mark the mouse back in the cage
-vehicle = {"baseline_start": -120, "baseline_end": -60} 
+drug = {"baseline_start": 120, "baseline_end": 60} #baseline is how much time before injection you want to measure before you mark the mouse back in the cage
+offset = 900
 
 #starting behavior marking your interest period
-start_behavior = "turning_novel"
+start_behavior = "cotton_saline"
 
 
 #vs nor_position = "Up"
